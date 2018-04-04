dc9d0d0331ab2266abc4d19302d8835eeb88a956
Joshua Styles <styles@outlook.com>
2018-04-04 03:54:39 +0100

    Responsive layouts complete
    Layouts of divs completed for all pages. Routing set up.  All pages
    adhere to responsive design.

--- styles-website/src/app/app-routing.module.ts
+++ styles-website/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'portfolio',
+        component: PortfolioComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
